8efe8798c323686dfa68748ba49e1f3b6e3cf08a
nilisav <vlad.vasilin.03@gmail.com>
2023-09-26 19:30:34 +0300

    fixed

--- tests/init_test.py
+++ tests/init_test.py
@@ -1,19 +1,19 @@
 from click.testing import CliRunner
-import sort_i.__init__
+import sort_i
 
 runner = CliRunner()
 def test_first():
-    result = runner.invoke(sort_i.__init__.testfunc, [])
+    result = runner.invoke(sort_i.testfunc, [])
     assert result.output == "[]\n"
 
 def test_second():
-    result = runner.invoke(sort_i.__init__.testfunc, ["1", "2", "3"])
+    result = runner.invoke(sort_i.testfunc, ["1", "2", "3"])
     assert result.output == "[1, 2, 3]\n"
 
 def test_third():
-    result = runner.invoke(sort_i.__init__.testfunc, ["1", "5", "3", "8", "4"])
+    result = runner.invoke(sort_i.testfunc, ["1", "5", "3", "8", "4"])
     assert result.output == "[1, 3, 4, 5, 8]\n"
 
 def test_fourth():
-    result = runner.invoke(sort_i.__init__.testfunc, ["2", "5", "9", "1", "2"])
+    result = runner.invoke(sort_i.testfunc, ["2", "5", "9", "1", "2"])
     assert result.output == "[1, 2, 2, 5, 9]\n"
